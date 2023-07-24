0c9c5e834c3ed0f4f4e8ee2281ba4a500fd07119
Precise Goals <elitesarthakamr@gmail.com>
2023-07-24 12:59:00 +0530

    some changes

--- src/components/particles/ParticleBackground.jsx
+++ src/components/particles/ParticleBackground.jsx
@@ -5,10 +5,6 @@ import { loadFull } from "tsparticles";
 function App() {
   const particlesInit = async (main) => {
     console.log(main);
- 
-    // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
-    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-    // starting from v2 you can add only the features you need reducing the bundle size
     await loadFull(main);
   };
  
