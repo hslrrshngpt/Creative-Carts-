d8b1001355af8538b43a76daece6f742fc0a74eb
Precise Goals <elitesarthakamr@gmail.com>
2023-07-27 14:55:25 +0530

    Responsive created for smartphone and android and IOS specially

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -14,7 +14,6 @@ const Navbar = () => {
             <h5><a href="">Homepage</a></h5>
             <h5><a href="">Shopping and Servicesㅤ<FontAwesomeIcon icon={faCaretRight} className='caretdown'/> </a></h5>
             <h5><a href="">Contact Infoㅤ< FontAwesomeIcon icon={faCaretRight} className='caretdown'/></a></h5>
-            {/* Try applying react icons there */}
             <h5><a href="">FAQ's</a></h5>
             <h5><a href="">Login</a></h5>
         </div>
